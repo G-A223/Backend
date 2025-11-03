92fe22d71a19a60e46d949e113f3a3557045920e
G-A223 <gunko.anastassiya@yandex.ru>
2025-11-03 19:49:02 +1000

    Add API for booking system

---
+++ src/Controller/ReserveController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use App\Services\ServicesCSV;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ReserveController extends AbstractController
+{
+    private ServicesCSV $servicesCsv;
+
+    public function __construct(ServicesCSV $servicesCsv)
+    {
+        $this->servicesCsv = $servicesCsv;
+    }
+
+    #[Route('/reserve', name: "reserve", methods: ['POST'])]
+    public function reserve(Request $request): Response
+    {
+        $message = '';
+        $phone = $request->request->get('phone_number');
+        $id = $request->request->get('id');
+        $comment = $request->request->get('comment');
+
+        $data = array(0, $id, $phone, $comment);
+
+        try {
+            $this->servicesCsv->makeReservation('reservations.csv', $data);
+            $this->addFlash('Success', 'Заявка успешно создана!');
+        }
+        catch (\Exception $e) {
+            $this->addFlash('error', 'Ошибка: ' . $e->getMessage());
+
+        }
+
+        return $this->redirectToRoute('home');
+    }
+
+    #[Route('/reserve/{id}/edit', name: "edit", methods: ['GET', 'POST', 'PUT'])]
+    public function edit(Request $request, int $id): Response
+    {
+        $reservations = $this->servicesCsv->readCSV('reservations.csv');
+        $reservation = null;
+
+        foreach ($reservations as $reserv) {
+            if ($reserv[0] == $id) {
+                $reservation = $reserv;
+                break;
+            }
+        }
+
+        if ($request->isMethod('POST') && $request->request->get('_method') === 'PUT') {
+            $comment = $request->request->get('comment');
+            $reservation[3] = $comment;
+
+            try {
+                $this->servicesCsv->updateComment('reservations.csv', $comment, $id);
+                $this->addFlash('Success', 'Комментарий изменен');
+
+            }
+            catch (\Exception $e) {
+                $this->addFlash('error', 'Ошибка: ' . $e->getMessage());
+
+            }
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('edit.html.twig', [
+            'reservation' => $reservation,
+        ]);
+    }
+}
